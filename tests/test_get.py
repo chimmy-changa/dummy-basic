0b0b2391208571f661c19d86f4e80e71198780a8
Sourabh Kondapaka <skondapa@mathworks.com>
2021-08-27 06:52:06 +0530

    black formatting

--- tests/test_get.py
+++ tests/test_get.py
@@ -3,6 +3,7 @@ from dummy_basic.get import get_hello, get_hello_world
 
 def test_get_hello():
     assert "hello" == get_hello()
-    
+
+
 def test_get_hello_world():
     assert "hello_world" == get_hello_world()
